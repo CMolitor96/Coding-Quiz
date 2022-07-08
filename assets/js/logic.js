82f373ea6075291952eaf832e122408e20c47810
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-07-08 10:27:58 -0700

    Added html to get first look at landing page of website. Added css to style landing page of website. Added a function in js for countdown timer.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,18 @@
 // TODO: add game logic
+var startButton = document.querySelector('#start');
+
+//countdown timer
+var timeLeft = document.querySelector('#countdown');
+var secondsLeft = 60;
+function countdownTimer() {
+    var countdown = setInterval(function() {
+        secondsLeft--;
+        timeLeft.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            clearInterval(countdown);
+        }
+    }, 1000)
+}
+
+startButton.addEventListener('click', countdownTimer);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,2 +1,37 @@
 // TODO: store all questions here
 // NOTE: hint - can use an array that contains objects
+const questions = [
+    {Question: "Which of the following is not a primitive data type in Javascript?",
+    Choices: ['String', 'Number', 'Boolean', 'Array'],
+    Answer: 3,
+    },
+
+    {
+        Question: "An ID in HTML is represented by what symbol in Javascript?",
+        Choices: ['.', '#', '{}', '[]'],
+        Answer: 1,
+    },
+
+    {
+        Question: "A class in HTML is represented by what symbol in Javascript?",
+        Choices: ['.', '#', '{}', '[]'],
+        Answer: 0,
+    },
+
+    {
+        Question: "What year was Javascript invented?",
+        Choices: ['1992', '1993', '1994', '1995'],
+        Answer: 3,
+    },
+
+    {
+        Question: "What is the proper syntax for multi line comments in Javascript?",
+        Choices: ['<!-- -->', '//', '/* */', '#'],
+        Answer: 2,
+    },
+
+];
+
+
+
+console.log(questions[0]);
