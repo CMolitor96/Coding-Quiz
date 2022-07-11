9809ddd5ad7e0d3c3f29f108ae69258d792b42b6
Charles Molitor <Charlie@Charless-MacBook-Pro.local>
2022-07-11 11:46:17 -0700

    Added correct and incorrect features and sounds on clicks to option choices in first question. Need to add next questions and link all clicks to questions to establish flow

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,48 +3,40 @@ var startButton = document.querySelector('#start');
 
 //On time ===0 make enter score screen
 var resultsEl = document.querySelector('#results');
-var enterScore = document.createElement('input');
+var enterScore = document.createElement('input'); 
+var score = secondsLeft;
 
 function enterScore() {
+    questionEl.style.display = 'none';
+    choicesEl.style.display = 'none';
     var resultsEl = document.querySelector('#results');
     var score = secondsLeft;
     resultsEl.textContent('Times Up! Your score: ' + score);
     var inputScore = document.createElement('input');
     var submitScore = document.createElement('button');
     submitScore.textContent = 'Submit';
+    resultsEl.appendChild(inputScore);
+    resultsEl.appendChild(submitScore);
 
-    if (inputScore.length > 5) {
-        alert('Please only provide up to 4 characters for your initials');
-    }
+    // while (inputScore.length > 5) {
+    //     alert('Please only provide up to 4 characters for your initials');
+    // }
 }
 
 //countdown timer
 var timeLeft = document.querySelector('#countdown');
-var secondsLeft = 10;
+var secondsLeft = 60;
 function countdownTimer() {
     var countdown = setInterval(function() {
         secondsLeft--;
         timeLeft.textContent = secondsLeft;
 
         if (secondsLeft === 0) {
-            
-            
-                questionEl.style.display = 'none';
-                choicesEl.style.display = 'none';
-                var resultsEl = document.querySelector('#results');
-                var score = secondsLeft;
-                resultsEl.textContent = 'Times Up! Your score: ' + score;
-                var inputScore = document.createElement('input');
-                var submitScore = document.createElement('button');
-                submitScore.textContent = 'Submit';
-                resultsEl.appendChild(inputScore);
-                resultsEl.appendChild(submitScore);
-            
-                if (inputScore.length > 5) {
-                    alert('Please only provide up to 4 characters for your initials');
-                }
-            
             clearInterval(countdown);
+            timeLeft.textContent = '';
+        }
+        if (timeLeft.textContent === '') {
+            enterScore();
         }
     }, 1000)
 }
@@ -61,7 +53,7 @@ function hideMain() {
 function timerAndHideMain() {
     countdownTimer();
     hideMain();
-    displayQuestion();
+    displayQuestionOne();
 }
 
 var questionEl = document.querySelector('#displayQuestion');
@@ -74,20 +66,77 @@ var secondQuestion = questions[1];
 var thirdQuestion = questions[2];
 var fourthQuestion = questions[3];
 var fifthQuestion = questions[4];
-function displayQuestion() {
+
+var wrongAudio = document.querySelector('#wrongAudio');
+var correctAudio = document.querySelector('#correctAudio');
+
+function displayQuestionOne() {
     questionEl.textContent = firstQuestion.Question;
 
     for (var i = 0; i < 4; i++) {
         var liEl = document.createElement('li');
         liEl.textContent = firstQuestion.Choices[i];
         liEl.setAttribute('style', 'display: block; padding: 10px; border: 2px solid black; width: 150px; margin-left: auto; margin-right: auto; margin-top: 10px; margin-bottom: 10px;');
+        liEl.setAttribute('id', 'option');
         choicesEl.appendChild(liEl);
         choicesEl.setAttribute('style', 'text-align: center;');
     }
+
+    var optionOne = document.getElementsByTagName('li')[0];
+    var optionTwo = document.getElementsByTagName('li')[1];
+    var optionThree = document.getElementsByTagName('li')[2];
+    var optionFour = document.getElementsByTagName('li')[3];
+
+
+    // optionOne.addEventListener('click', wrongChoice);
+    // optionTwo.addEventListener('click', wrongChoice);
+    // optionThree.addEventListener('click', wrongChoice);
+
+    var wrongAudio = document.querySelector('#wrongAudio');
+    var correctAudio = document.querySelector('#correctAudio');
+    function playWrongAudio() {
+        wrongAudio.play();
+    }
+
+
+    function wrongOne() {
+        optionOne.setAttribute('style', 'display: block; padding: 10px; border: 2px solid black; width: 150px; margin-left: auto; margin-right: auto; margin-top: 10px; margin-bottom: 10px; background-color: red;');
+        // function playWrongAudio() {
+        //     wrongAudio.play();
+        // }
+        playWrongAudio();
+    }
+    function wrongTwo() {
+        optionTwo.setAttribute('style', 'display: block; padding: 10px; border: 2px solid black; width: 150px; margin-left: auto; margin-right: auto; margin-top: 10px; margin-bottom: 10px; background-color: red;');
+        playWrongAudio();
+    }
+    function wrongThree() {
+        optionThree.setAttribute('style', 'display: block; padding: 10px; border: 2px solid black; width: 150px; margin-left: auto; margin-right: auto; margin-top: 10px; margin-bottom: 10px; background-color: red;');
+        playWrongAudio();
+    }
+    function right() {
+        optionFour.setAttribute('style', 'display: block; padding: 10px; border: 2px solid black; width: 150px; margin-left: auto; margin-right: auto; margin-top: 10px; margin-bottom: 10px; background-color: green;');
+        function playCorrectAudio() {
+            correctAudio.play();
+        }
+        playCorrectAudio();
+    }
+
+
+    optionOne.addEventListener('click', wrongOne);
+    optionTwo.addEventListener('click', wrongTwo);
+    optionThree.addEventListener('click', wrongThree);
+    optionFour.addEventListener('click', right);
     
+    
+
 }
 
 
 // startButton.addEventListener('click', hideMain);
 // startButton.addEventListener('click', countdownTimer);
 startButton.addEventListener('click', timerAndHideMain);
+if (timeLeft.textContent === '') {
+    console.log("Hello");
+}
+
